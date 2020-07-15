ae6804c6068dd374a45549609ddcaf0a39bc94f0
Mykhailo Zub <mikhail.a.zub@gmail.com>
2020-07-15 12:50:07 +0300

    mistake correction and style apply

--- HW1-Variables/script.js
+++ HW1-Variables/script.js
@@ -1,75 +1,113 @@
-let sugarPrice = 15.678;
-let vinePrice = 123.965;
-let meetPrice = 90.2345;
+const sugarPrice = 15.678;
+const vinePrice = 123.965;
+const meetPrice = 90.2345;
 
-let maxPrice = Math.max(sugarPrice, vinePrice, meetPrice);
+const maxPrice = Math.max(sugarPrice, vinePrice, meetPrice);
 
 console.log(maxPrice);
 
-let minPrice = Math.min(sugarPrice, vinePrice, meetPrice);
+const minPrice = Math.min(sugarPrice, vinePrice, meetPrice);
 
 console.log(minPrice);
 
-let summOfProducts = sugarPrice + vinePrice + meetPrice;
+const summOfProducts = sugarPrice + vinePrice + meetPrice;
 
 console.log(summOfProducts);
 
-let summToInteger = Math.floor(sugarPrice) + Math.floor(vinePrice) + Math.floor(meetPrice);
+const summToInteger = Math.floor(sugarPrice) + Math.floor(vinePrice) + Math.floor(meetPrice);
 
 console.log(summToInteger);
 
-let summTo100 = Math.round(Math.round(summOfProducts)/100) * 100;
+const summTo100 = Math.round(Math.round(summOfProducts)/100) * 100;
 
 console.log(summTo100);
 
-let isEven;
+const isEven = summOfProducts % 2 === 0 ? true : false;
 
-if (summOfProducts % 2) {
-    isEven = true; 
-    console.log(isEven);
-} else {
-    isEven = false;
-    console.log(isEven);
-}
+console.log(isEven);
 
-let summOfChangeFromSugar = 500 - sugarPrice;
-let summOfChangeFromVine = 500 - vinePrice;
-let summOfChangeFromMeet = 500 - meetPrice;
+const summOfChangeFromSugar = 500 - sugarPrice;
+const summOfChangeFromVine = 500 - vinePrice;
+const summOfChangeFromMeet = 500 - meetPrice;
 
 console.log(summOfChangeFromSugar);
 console.log(summOfChangeFromVine);
 console.log(summOfChangeFromMeet);
 
-let averagePrice = Math.round(summOfProducts / 3 * 100) / 100;
+const averagePrice = Math.round(summOfProducts / 3 * 100) / 100;
 
 console.log(averagePrice);
 
-let discount = Math.round(Math.random() * 100) / 100;
+const discount = Math.round(Math.random() * 100) / 100;
 
 console.log(discount);
 
-let summWithDiscount = Math.round((summOfProducts - (summOfProducts * discount)) * 100) / 100;
+const summWithDiscount = Math.round((summOfProducts - (summOfProducts * discount)) * 100) / 100;
 
 console.log(summWithDiscount);
 
-let profitAll = summOfProducts / 2 - (summOfProducts - summWithDiscount);
+const profitAll = summOfProducts / 2 - (summOfProducts - summWithDiscount);
 
 console.log (profitAll);
 
-let results = `
-    <p>Max price: ${maxPrice}</p>
-    <p>Min price: ${minPrice}</p>
-    <p>Summ of all products: ${summOfProducts}</p>
-    <p>Summ to integer: ${summToInteger}</p>
-    <p>Summ to hundreds: ${summTo100}</p>
-    <p>Summ of all products even? ${isEven}</p>
-    <p>Change from 500 for sugar: ${summOfChangeFromSugar}</p>
-    <p>Change from 500 for vine: ${summOfChangeFromVine}</p>
-    <p>Change from 500 for meet: ${summOfChangeFromMeet}</p>
-    <p>Avarage price: ${averagePrice}</p>
-    <p>Discount: ${Math.round(discount * 100)}%</p>
-    <p>Summ with discount: ${summWithDiscount}</p>
-    <p>Profit: ${profitAll}</p>
+const results = `
+    <table>
+        <tr>
+            <caption>Summary table</caption>
+        </tr>   
+        <tr>
+            <td class="name">Max price</td>
+            <td class="value">${maxPrice}</td>
+        </tr>
+        <tr>
+            <td class="name">Min price</td>
+            <td class="value">${minPrice}</td>
+        </tr>
+        <tr>
+            <td class="name">Summ of all products</td>
+            <td class="value">${summOfProducts}</td>
+        </tr>
+        <tr>
+            <td class="name">Summ to integer</td>
+            <td class="value">${summToInteger}</td>
+        </tr>
+        <tr>
+            <td class="name">Summ to hundreds</td>
+            <td class="value">${summTo100}</td>
+        </tr>
+        <tr>
+            <td class="name">Summ of all products even?</td>
+            <td class="value">${isEven}</td>
+        </tr>
+        <tr>
+            <td class="name">Change from 500 for sugar</td>
+            <td class="value">${summOfChangeFromSugar}</td>
+        </tr>
+        <tr>
+            <td class="name">Change from 500 for vine</td>
+            <td class="value">${summOfChangeFromVine}</td>
+        </tr>
+        <tr>
+            <td class="name">Change from 500 for meet</td>
+            <td class="value">${summOfChangeFromMeet}</td>
+        </tr>
+        <tr>
+            <td class="name">Avarage price</td>
+            <td class="value">${averagePrice}</td>
+        </tr>
+        <tr>
+            <td class="name">Discount</td>
+            <td class="value">${Math.round(discount * 100)}%</td>
+        </tr>
+        <tr>
+            <td class="name">Summ with discount</td>
+            <td class="value">${summWithDiscount}</td>
+        </tr>
+        <tr>
+            <td class="name">Profit</td>
+            <td class="value">${profitAll}</td>
+        </tr>
+    </table>
 `;
 
 document.getElementById("results").innerHTML = results;
