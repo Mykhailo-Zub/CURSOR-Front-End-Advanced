5ca4ad44b557cf9cda513b181813ca6c1a05cc73
Mykhailo Zub <mikhail.a.zub@gmail.com>
2020-07-31 12:23:41 +0300

    func#2 is done

--- HW5-Arrays.methods/index.js
+++ HW5-Arrays.methods/index.js
@@ -12,43 +12,42 @@ console.log(getRandomArray(15, 1, 100));
 
 // 2
 
-// const getModa = (...numbers) => {
-//     console.log(numbers);
-//     let modaCount = numbers.sort((a, b) => a - b).map((item) => parseInt(item));
-//     const number = [];
-//     const count = [];
-//     for (let i = modaCount.length - 1, k = 0; i > 0; i--) {
-//         debugger;
-//         for (let y = 0; y <= number.length; y++) {
-//             if (modaCount[i] === number[y]) {
-//             count[y] = count[y] + 1;
-//             modaCount = modaCount.slice(0, i);
-//             i--;
-//             break;
-//             }
-//         for (let j = i - 1; j >= 0; j--) {       
-//             if (modaCount[i] === modaCount[j]) {
-//                 number[k] = modaCount[i];
-//                 count[k] = 1;
-//                 k++;
-//                 modaCount = modaCount.slice(0, i);
-//                 i--;
-//                 } else  {
-                    
-//             }
-//         }
-//     } 
-    
-// }console.log(count);
-//  return number;
-// }
+const modaCount = (arr) => {
+    let newArr = [...arr];
+    let numbers = [];
+    let answer = [];
+    do {
+        if (numbers.length !== 0) {
+            newArr = [...numbers]
+            numbers = [];
+        } for (let i = newArr.length - 1; i >= 0 ; i--) {
+        if (newArr[i - 1] === undefined) {
+        } else if (newArr[i] === newArr[i - 1]) {
+            numbers.push(newArr[i]);
+        }}}
+    while (numbers.length > 2);
+    if (numbers.length === 1) {
+        answer = [...numbers];
+    } else if (numbers[0] === numbers[1]) {
+        answer = [numbers[0]]
+    } else {
+        answer = [`This row of numbers is multimodal`];
+    }
+     return answer;
+}
 
-// console.log(getModa(1,5,7,3,5,4,1,1,7));
+const getModa = (...numbers) => {
+    let filteredNumbers = numbers.sort((a, b) => a - b).filter((item) => (item ^ 0) === item);
+    let answer = modaCount(filteredNumbers);
+    return answer;   
+}
+
+console.log(getModa(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
 
 // 3
 
 const getAverage = (...numbers) => {
-    numbers = numbers.filter((item) => (item !== isNaN && item !== undefined && typeof(item) === 'number'));
+    numbers = numbers.filter((item) => (item ^ 0) === item);
     const answer = numbers.reduce((acc, number) => acc + parseInt(number)) / numbers.length;
     return answer;
 }
@@ -58,7 +57,8 @@ console.log(getAverage(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
 // 4
 
 const getMedian = (...numbers) => {
-    numbers = numbers.filter((item) => (item !== isNaN && item !== undefined && typeof(item) === 'number'));
+    numbers = numbers.filter((item) => (item ^ 0) === item);
+    console.log(numbers);
     numbers.sort((a, b) => a - b).map((item) => parseInt(item));
     let answer = [];
     numbers.length % 2 === 0 
@@ -132,7 +132,7 @@ console.log(divideByThree("Commander"));
 
 const results = `
 <p>1. Get random array: ${getRandomArray(15, 1, 100)}</p>
-<p>2. Get moda: <span class="undone">Function is not ready</span></p>
+<p>2. Get moda:  ${getModa(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2)}</p>
 <p>3. Get average: ${getAverage(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2)}</p>
 <p>4. Get median: ${getMedian(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2)}</p>
 <p>5. Filter even numbers: ${filterEvenNumbers(1, 2, 3, 4, 5, 6)}</p>
