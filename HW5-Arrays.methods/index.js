c00d06451f325ffa7c55422e5b74b7461b296690
Mykhailo Zub <mikhail.a.zub@gmail.com>
2020-08-03 17:50:30 +0300

    mistake correction

--- HW5-Arrays.methods/index.js
+++ HW5-Arrays.methods/index.js
@@ -17,11 +17,11 @@ const modaCount = (arr) => {
     let numbers = [];
     let answer = [];
     do {
-        if (numbers.length !== 0) {
+        if (numbers.length) {
             newArr = [...numbers]
             numbers = [];
         } for (let i = newArr.length - 1; i >= 0 ; i--) {
-        if (newArr[i - 1] === undefined) {
+        if (!newArr[i - 1]) {
         } else if (newArr[i] === newArr[i - 1]) {
             numbers.push(newArr[i]);
         } else {
@@ -30,8 +30,8 @@ const modaCount = (arr) => {
     while (numbers.length > 2);
     if (numbers.length === 1) {
         answer = [...numbers];
-    } else if (numbers[0] === numbers[1] && numbers[0] !== undefined) {
-        answer = [numbers[0]]
+    } else if (numbers[0] === numbers[1] && numbers[0]) {
+        answer = numbers[0];
     } else {
         answer = [`This row of numbers is multimodal`];
     }
@@ -49,8 +49,8 @@ console.log(getModa(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
 // 3
 
 const getAverage = (...numbers) => {
-    numbers = numbers.filter((item) => (item ^ 0) === item);
-    const answer = numbers.reduce((acc, number) => acc + parseInt(number)) / numbers.length;
+    newNumbers = numbers.filter((item) => (item ^ 0) === item);
+    const answer = newNumbers.reduce((acc, number) => acc + parseInt(number), 0) / newNumbers.length;
     return answer;
 }
 
@@ -59,13 +59,12 @@ console.log(getAverage(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
 // 4
 
 const getMedian = (...numbers) => {
-    numbers = numbers.filter((item) => (item ^ 0) === item);
-    console.log(numbers);
-    numbers.sort((a, b) => a - b).map((item) => parseInt(item));
+    newNumbers = numbers.filter((item) => (item ^ 0) === item);
+    newNumbers.sort((a, b) => a - b).map((item) => parseInt(item));
     let answer = [];
-    numbers.length % 2 === 0 
-    ? answer = (numbers[(numbers.length / 2)] + numbers[((numbers.length / 2) - 1)]) / 2
-    : answer = numbers[Math.floor(numbers.length / 2)]
+    newNumbers.length % 2 === 0 
+    ? answer = (newNumbers[(newNumbers.length / 2)] + newNumbers[((newNumbers.length / 2) - 1)]) / 2
+    : answer = newNumbers[Math.floor(newNumbers.length / 2)]
     
     return answer;
 }
@@ -74,66 +73,66 @@ console.log(getMedian(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
 
 // 5
 
-const filterEvenNumbers = (...numbers) => {
-    numbers = numbers.filter((number) => number % 2 !== 0);
-    return numbers;
-}
+const filterEvenNumbers = (...numbers) => numbers.filter((number) => number % 2 !== 0);
 
 console.log(filterEvenNumbers(1, 2, 3, 4, 5, 6));
 
 //6
 
-const countPositiveNumbers = (...numbers) => {
-    numbers = numbers.filter((number) => number > 0);
-    return numbers.length;
-}
+const countPositiveNumbers = (...numbers) => numbers.filter((number) => number > 0).length;
 
 console.log(countPositiveNumbers(1, -2, 3, -4, -5, 6));
 
 // 7
 
-const getDividedByFive = (...numbers) => {
-    numbers = numbers.filter((number) => number % 5 === 0);
-    return numbers;
-}
+const getDividedByFive = (...numbers) => numbers.filter((number) => number % 5 === 0);
 
 console.log(getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
 
 // 8
 
 const replaceBadWords = (string) => {
+    let newString = string;
     const badWords = ['shit', 'fuck'];
         for (let i = 0; i < badWords.length; i++) {
-        if (string.includes(badWords[i])) {
-            string = string.replace(badWords[i], '****');
-        }} return string;
+        while (newString.toLowerCase().includes(badWords[i])) {
+            newString = newString.replace(new RegExp(`${badWords[i]}`, `ig`), '****');
+        }} return newString;
     }
 
-console.log(replaceBadWords("It's bullshit!"));
+console.log(replaceBadWords("It's bullshit! Fuck."));
  
 // 9
 
-const divideByThree = (word) => {
-    word = word.toLowerCase();
-    while (word.includes(' ')) {
-        word = word.replace(' ', '');
-    }
-    const syllables = [];
-    console.log(word);
-    for (let i = 0, j = 0; i < (word.length / 3); i++) {
-        if (word[j + 2] === undefined) {
-            if (word[j + 1] === undefined) {
-                syllables[i] = [word[j]];
-                break;
-            } syllables[i] = [word[j] + word[j + 1]];
-            j += 2;
-            break;
-        } syllables[i] = [word[j] + word[j + 1] + word[j + 2]];
-        j += 3;
-    } return syllables;
-}
+// const divideByThree = (word) => {
+//     let newWord = word.toLowerCase();
+//     while (newWord.includes(' ')) {
+//         newWord = newWord.replace(' ', '');
+//     }
+//     const syllables = [];
+//     for (let i = 0, j = 0; i < (newWord.length / 3); i++) {
+//         if (!newWord[j + 2]) {
+//             if (!newWord[j + 1]) {
+//                 syllables[i] = [newWord[j]];
+//                 break;
+//             } syllables[i] = [newWord[j] + newWord[j + 1]];
+//             j += 2;
+//             break;
+//         } syllables[i] = [newWord[j] + newWord[j + 1] + newWord[j + 2]];
+//         j += 3;
+//     } return syllables;
+// }
 
-console.log(divideByThree("Commander"));
+// 9 (Max edition)
+
+const divideByThree = (word, value) => word.toLowerCase().replace(/\s/g, '').split('').reduce((acc, el, i, arr) => {
+        if (i % value === 0) {
+            const tempArr = arr.slice(i, i + value).join('');
+            acc.push(tempArr);
+        } return acc
+    },[]);
+
+console.log(divideByThree("Commander", 3));
 
 const results = `
 <p>1. Get random array: ${getRandomArray(15, 1, 100)}</p>
@@ -143,8 +142,8 @@ const results = `
 <p>5. Filter even numbers: ${filterEvenNumbers(1, 2, 3, 4, 5, 6)}</p>
 <p>6. Count positive numbers: ${countPositiveNumbers(1, -2, 3, -4, -5, 6)}</p>
 <p>7. Get divided by five: ${getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2)}</p>
-<p>8. Replace bad words: ${replaceBadWords("Are you fucking kidding?")}</p>
-<p>9. Divide by three: ${divideByThree("Commander")}</p>
+<p>8. Replace bad words: ${replaceBadWords("Are you fucking kidding? It's bullshit!")}</p>
+<p>9. Divide by three: ${divideByThree("Commander", 3)}</p>
 <p>10. Generate combinations: <span class="undone">Function is not ready</span></p>`;
 
 document.querySelector('.results').innerHTML = results;
