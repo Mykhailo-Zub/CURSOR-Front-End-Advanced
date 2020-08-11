3b2f58656bc895c8497124768be21a1e3e239679
Mykhailo Zub <mikhail.a.zub@gmail.com>
2020-08-11 18:08:25 +0300

    hw9 ready

---
+++ HW9-DOM/index.js
@@ -0,0 +1,40 @@
+const getRandom = () => Math.round((255 - 0) * Math.random() + 0);
+
+const showBlocks = () =>
+    document.querySelector(".blocks-wrapper").classList.toggle("hidden");
+
+const changeColor = () => {
+    document
+        .querySelectorAll(".block")
+        .forEach(
+            (item) =>
+                (item.style.backgroundColor = `rgb(${getRandom()}, ${getRandom()}, ${getRandom()})`)
+        );
+};
+
+const start = () => {
+    changeColor();
+    showBlocks();
+    if (
+        document.querySelector(".blocks-wrapper").classList.contains("hidden")
+    ) {
+        document.getElementById("start-btn").innerHTML = "Show blocks";
+    } else {
+        document.getElementById("start-btn").innerHTML = "Hide blocks";
+    }
+};
+
+let interval;
+let switcher = false;
+
+const changer = () => {
+    if (switcher) {
+        clearInterval(interval);
+        switcher = false;
+        document.getElementById("change-btn").innerHTML = "Start color changer";
+    } else {
+        interval = setInterval(() => changeColor(), 1000);
+        switcher = true;
+        document.getElementById("change-btn").innerHTML = "Stop color changer";
+    }
+};
