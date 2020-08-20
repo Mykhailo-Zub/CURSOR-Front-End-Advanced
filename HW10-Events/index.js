2a180ace02ba52c09d0325ed7128035b2d4c3e25
Mykhailo Zub <mikhail.a.zub@gmail.com>
2020-08-20 18:45:37 +0300

    code optimization

--- HW10-Events/index.js
+++ HW10-Events/index.js
@@ -8,71 +8,54 @@ function addBlack(btn) {
     setTimeout(() => btn.classList.remove("black-pressed"), 300);
 }
 
-function playSound(sound) {
+function playSoundAndPress(link) {
+    const sound = new Audio(`sound/1(${link}).mp3`);
     sound.play();
+    link < 5
+        ? addWhite(document.querySelector(`.btn${link}`))
+        : addBlack(document.querySelector(`.btn${link}`));
 }
 
-const whiteButtonsForMouse = document.querySelectorAll(".white-btn");
-const blackButtonsForMouse = document.querySelectorAll(".black-btn");
+const ButtonsForMouse = document.querySelectorAll(".btn");
 
-whiteButtonsForMouse.forEach((elem, index) => {
+ButtonsForMouse.forEach((elem, index) => {
     elem.addEventListener("click", () => {
-        addWhite(elem);
-        playSound(new Audio(`sound/1(${index}).mp3`));
+        playSoundAndPress(index);
     });
 });
 
-blackButtonsForMouse.forEach((elem, index) => {
-    elem.addEventListener("click", () => {
-        addBlack(elem);
-        playSound(new Audio(`sound/1(${index + 5}).mp3`));
-    });
-});
-
-const whiteButtonsForKeyboard = whiteButtonsForMouse.forEach((elem, index) => {
-    elem.classList.add(`white${index}`);
-});
-const blackButtonsForKeyboard = blackButtonsForMouse.forEach((elem, index) => {
-    elem.classList.add(`black${index}`);
+const ButtonsForKeyboard = ButtonsForMouse.forEach((elem, index) => {
+    elem.classList.add(`btn${index}`);
 });
 
 document.body.addEventListener("keydown", function (event) {
     switch (event.keyCode) {
         case 65:
-            addWhite(document.querySelector(".white0"));
-            playSound(new Audio(`sound/1(0).mp3`));
+            playSoundAndPress(0);
             break;
         case 83:
-            addBlack(document.querySelector(".black0"));
-            playSound(new Audio(`sound/1(5).mp3`));
+            playSoundAndPress(5);
             break;
         case 68:
-            addWhite(document.querySelector(".white1"));
-            playSound(new Audio(`sound/1(1).mp3`));
+            playSoundAndPress(1);
             break;
         case 70:
-            addBlack(document.querySelector(".black1"));
-            playSound(new Audio(`sound/1(6).mp3`));
+            playSoundAndPress(6);
             break;
         case 71:
-            addWhite(document.querySelector(".white2"));
-            playSound(new Audio(`sound/1(2).mp3`));
+            playSoundAndPress(2);
             break;
         case 72:
-            addBlack(document.querySelector(".black2"));
-            playSound(new Audio(`sound/1(7).mp3`));
+            playSoundAndPress(7);
             break;
         case 74:
-            addWhite(document.querySelector(".white3"));
-            playSound(new Audio(`sound/1(3).mp3`));
+            playSoundAndPress(3);
             break;
         case 75:
-            addBlack(document.querySelector(".black3"));
-            playSound(new Audio(`sound/1(8).mp3`));
+            playSoundAndPress(8);
             break;
         case 76:
-            addWhite(document.querySelector(".white4"));
-            playSound(new Audio(`sound/1(4).mp3`));
+            playSoundAndPress(4);
             break;
     }
 });
