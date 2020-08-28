6fc99504618ba81baf3a12f66e9a2ff3997264a9
Mykhailo Zub <mikhail.a.zub@gmail.com>
2020-08-28 14:08:45 +0300

    hw13 ready

---
+++ HW13-Generators/index.js
@@ -0,0 +1,54 @@
+function* createIdGenerator() {
+    for (let i = 1; i >= 0; i++) yield i;
+}
+
+const idGenerator = createIdGenerator();
+
+document.getElementById("id-gen").addEventListener("click", () => {
+    const container = document.querySelector(".results");
+    let newId = document.createElement("span");
+    newId.innerHTML = ` ${idGenerator.next().value};`;
+    container.append(newId);
+});
+
+let fontSize = 14;
+
+function* createFontGenerator(defaultSize) {
+    fontSize = defaultSize;
+    while (fontSize) {
+        let change = yield;
+        if (change === "up") {
+            fontSize += 2;
+        }
+        if (change === "down") {
+            if (fontSize < 4) {
+                fontSize;
+            } else {
+                fontSize -= 2;
+            }
+        } else {
+            fontSize;
+        }
+    }
+}
+
+const fontGenerator = createFontGenerator(14);
+
+const font = document.querySelector(".font-for-change");
+
+document.getElementById("default").addEventListener("click", () => {
+    fontSize = 14;
+    font.style.fontSize = `${fontSize}px`;
+});
+
+document.getElementById("up").addEventListener("click", () => {
+    fontGenerator.next();
+    fontGenerator.next("up");
+    font.style.fontSize = `${fontSize}px`;
+});
+
+document.getElementById("down").addEventListener("click", () => {
+    fontGenerator.next();
+    fontGenerator.next("down");
+    font.style.fontSize = `${fontSize}px`;
+});
