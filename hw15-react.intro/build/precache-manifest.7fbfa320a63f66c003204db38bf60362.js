0fc08d1b21847f5205b396c5f7068589a5a58b13
Mykhailo Zub <mikhail.a.zub@gmail.com>
2020-09-06 10:48:39 +0300

    hw15 ready

---
+++ hw15-react.intro/build/precache-manifest.7fbfa320a63f66c003204db38bf60362.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91ed5ba5d31cb96149efd14f983e2ba8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17939794a5c86dcb41ab",
+    "url": "/static/css/main.0b3dab1c.chunk.css"
+  },
+  {
+    "revision": "da01dc2f6e18290ac7f9",
+    "url": "/static/js/2.60e1fce5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.60e1fce5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17939794a5c86dcb41ab",
+    "url": "/static/js/main.e103974a.chunk.js"
+  },
+  {
+    "revision": "f9ca20a0f292f26d78de",
+    "url": "/static/js/runtime-main.541e901b.js"
+  },
+  {
+    "revision": "0002e5f6fa980eed02d3897ecc5eb36d",
+    "url": "/static/media/comment.0002e5f6.svg"
+  },
+  {
+    "revision": "39726703564a2a71e8077948ac5d964c",
+    "url": "/static/media/confirm.39726703.svg"
+  },
+  {
+    "revision": "da3338122f87907c1fb994f17002f866",
+    "url": "/static/media/down-arrow.da333812.svg"
+  },
+  {
+    "revision": "12220a2259c8f1718847197c4bfdb84e",
+    "url": "/static/media/like.12220a22.svg"
+  },
+  {
+    "revision": "444db2463624b56ae55674c6e654d9b9",
+    "url": "/static/media/share.444db246.svg"
+  },
+  {
+    "revision": "8f047106898ba92d2dc7e03f29f16bed",
+    "url": "/static/media/upload.8f047106.svg"
+  }
+]);
--- hw15-react.intro/src/components/post.js
+++ hw15-react.intro/src/components/post.js
@@ -1,23 +1,48 @@
 import React from "react";
+import confirmImg from "../img/confirm.svg";
+import downArrowImg from "../img/down-arrow.svg";
+import commentImg from "../img/comment.svg";
+import shareImg from "../img/share.svg";
+import likeImg from "../img/like.svg";
+import uploadImg from "../img/upload.svg";
 
 const Post = (props) => {
     return (
-        <div class="post-wrapper">
+        <div className="post-wrapper">
             <div
-                class="avatar"
+                className="avatar"
                 style={{ backgroundImage: `url(${props.author.photo})` }}
             ></div>
-            <div class="content-column">
+            <div className="content-column">
                 <div className="name-row">
                     <div className="name">{props.author.name}</div>
-                    <img src="" alt="" className="confirm" />
+                    <img src={confirmImg} alt="" className="confirm" />
                     <div className="nickname">{props.author.nickname}</div>
                     <div className="dot"></div>
                     <div className="date">{props.date}</div>
-                    <div className="more-arrow"></div>
+                    <img className="more-arrow" src={downArrowImg} alt="" />
                 </div>
                 <div className="content">{props.content}</div>
-                <div className="content-img">{props.image}</div>
+                <div className="content-img">
+                    <img src={props.image} alt="" />
+                </div>
+                <div className="social-btns">
+                    <div>
+                        <img src={commentImg} alt="" />
+                        <span>34</span>
+                    </div>
+                    <div>
+                        <img src={shareImg} alt="" />
+                        <span>8</span>
+                    </div>
+                    <div>
+                        <img src={likeImg} alt="" />
+                        <span>165</span>
+                    </div>
+                    <div>
+                        <img src={uploadImg} alt="" />
+                    </div>
+                </div>
             </div>
         </div>
     );
